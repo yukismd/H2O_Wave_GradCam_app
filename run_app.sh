0a6e240c4893d52d33ac614addb44948589287f6
嶌田有希 <YShimada-MBP16@shimadayukinoMacBook-Pro.local>
2021-08-22 09:44:18 +0900

    div app.py

--- run_app.sh
+++ run_app.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 # Waveサーバ上にアップされたデータの削除
-rm -rf $HOME/wave-0.13.0-linux-amd64/data/f/*
+rm -rf $HOME/wave-0.17.0-linux-amd64/data/f/*
 
 # Waveサーバの起動
-cd wave-0.13.0-linux-amd64
+cd wave-0.17.0-linux-amd64
 nohup ./waved &
 echo "Wave server UP!!"
 
 # アプリの起動
-cd ~/app_CreditCardScoring_v2
-#nohup wave0130/bin/wave run sys_monitoring.py &
+cd ~/wave_app
 nohup wave0130/bin/python sys_monitoring2.py &
-nohup wave0130/bin/wave run credit_scoring3.py &
-#source wave0130/bin/activate
-#nohup wave run sys_monitoring.py &
-#nohup wave run credit_scoring3.py &
+nohup wave0130/bin/wave run app.py &
+
 echo "App is running!!"
 
---
+++ wave_app/app.py
@@ -0,0 +1,120 @@
+"""
+各モデルは、App実行の前にrun_example.shを一度実施しておかなくてはいけない
+
+[2021/8/xx] version 1
+"""
+
+import os
+import subprocess
+import zipfile
+from h2o_wave import Q, main, app, ui, handle_on, on, data
+import pandas as pd
+import numpy as np
+
+
+"""
+q.client.model_selected
+q.client.selected_model
+q.client.model_path
+q.client.data_path_input
+q.client.data_path_output
+q.client.data_path_input_folder
+"""
+
+def run_python_scoring_pipeline():
+    """ model_path上のPythonScoringPipelineを実行
+    """
+    #(todo) data_path_inputを指定して、スコアリング
+    # data_path_output上に結果（GradCAM Image folder, 予測結果csv）が保存される
+
+    return 'Scoring Done!!!'
+
+
+@app('/app')
+async def serve(q: Q):
+
+    q.page['header'] = ui.header_card(
+        box='1 1 10 1',    # x座標 y座標 幅 高さ
+        title='Grad-CAM Scoring',
+        subtitle='Image Model (Python Scoring) of Driverless AI',
+    )
+
+    if not q.client.model_selected:    # 初期画面。モデル選択前
+        list_model_path = os.listdir(os.path.join('..', 'models'))
+        choices = [ui.choice(mod, mod) for mod in list_model_path]
+        q.page['card_model_select'] = ui.form_card(
+            box='1 2 2 10',    # x座標 y座標 幅 高さ
+            items=[
+                ui.text_xl('Select a model'),
+                ui.dropdown(name='button_selected_model', label='Which model?', value='', required=True, choices=choices),
+                ui.button(name='button_model_selected', label='Select This Model', primary=True),
+            ]
+        )
+    
+    await handle_on(q)     # 各ボタンが押された場合、発動
+
+    print("q.args --> ", q.args)
+    print("q.client --> ", q.client)
+    await q.page.save()
+
+
+@on('button_model_selected')
+async def model_selected(q: Q):
+    """ モデル選択後、ファイルのアップ画面
+    """
+    #(todo) 以下の場合(モデルが選択されていない)、中止
+    # if q.args.button_selected_model=='':
+
+    del q.page['card_model_select'], q.page['card_result']
+    q.client.model_selected = True
+    q.client.selected_model = q.args.button_selected_model
+    q.client.model_path = os.path.join('..', 'models', q.client.selected_model)
+    q.client.data_path_input = os.path.join(q.client.model_path, 'data', 'input_data')
+    os.makedirs(q.client.data_path_input, exist_ok=True)
+    q.client.data_path_output = os.path.join(q.client.model_path, 'data', 'output_data')
+    os.makedirs(q.client.data_path_output, exist_ok=True)
+
+    q.page['card_data_upload'] = ui.form_card(
+        box='1 2 2 10',    # x座標 y座標 幅 高さ
+        items=[
+            ui.text_xl('Selected model: {}'.format(q.client.selected_model)),
+            ui.text_l('Upload the data'),
+            ui.file_upload(name='button_file_uploaded', 
+                        label='Upload this file & Do scoring', 
+                        multiple=True,
+                        file_extensions=['zip'],   # 許可するファイル形式
+                        max_file_size=10, 
+                        max_size=15
+            )
+        ]
+    )
+
+
+@on('button_file_uploaded')
+async def do_scoring(q: Q):
+    """ 結果の表示
+    """
+    del q.page['card_model_select']
+    print('Uploaded zip on wave server: {}'.format(q.args.button_file_uploaded))
+
+    local_path_zip = await q.site.download(q.args.button_file_uploaded[0], path=q.client.data_path_input)  # Waveサーバ上のデータをApp実行パス上にロード、そのパスの取得
+    print(local_path_zip)
+    print(os.path.splitext(os.path.basename(local_path_zip)))
+    with zipfile.ZipFile(local_path_zip) as existing_zip:
+        existing_zip.extractall(q.client.data_path_input)
+
+    # 解凍フォルダのパス
+    q.client.data_path_input_folder = os.path.join(q.client.data_path_input, os.path.splitext(os.path.basename(local_path_zip))[0])
+    
+    res = run_python_scoring_pipeline()   # PyScoringの実行
+    print(res)
+
+    q.page['card_result'] = ui.form_card(
+        box='3 2 4 10',    # x座標 y座標 幅 高さ
+        items=[
+            ui.text_xl('Scoring Result'),
+            #ui.text_m('{}'.format()),
+            #(todo) GradCAM結果の表示
+            #(todo) 予測結果のDL
+        ]
+    )
