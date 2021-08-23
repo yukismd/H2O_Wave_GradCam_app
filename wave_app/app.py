1edeed1fc1be9b5560311e8460987bbdf6d580a2
yukismd <yuki.smd@gmail.com>
2021-08-23 02:00:44 +0000

    app.py

--- wave_app/app.py
+++ wave_app/app.py
@@ -1,10 +1,11 @@
 """
-各モデルは、App実行の前にrun_example.shを一度実施しておかなくてはいけない
+各モデルは、App実行の前に、models内に配置したPythonScoringディレクトリ上で環境構築（run_example.shが実行できる状態）しておかなくてはならない
 
 [2021/8/xx] version 1
 """
 
 import os
+import glob
 import subprocess
 import zipfile
 from h2o_wave import Q, main, app, ui, handle_on, on, data
@@ -12,19 +13,21 @@ import pandas as pd
 import numpy as np
 
 
+
 """
 q.client.model_selected
 q.client.selected_model
 q.client.model_path
-q.client.data_path_input
-q.client.data_path_output
-q.client.data_path_input_folder
+q.client.data_path_input : '../models/art_age/data/input_data'
+q.client.data_path_input_folder : zipから展開したフォルダ名 '../models/art_age/data/input_data/sample1'
+q.client.data_path_output : '../models/art_age/data/output_data'
+q.client.data_path_output_folder : 結果の保存先 '../models/art_age/data/output_data/sample1'
 """
 
 def run_python_scoring_pipeline():
     """ model_path上のPythonScoringPipelineを実行
     """
-    #(todo) data_path_inputを指定して、スコアリング
+    #(todo) data_path_input_folder内の写真をスコアリング
     # data_path_output上に結果（GradCAM Image folder, 予測結果csv）が保存される
 
     return 'Scoring Done!!!'
@@ -40,7 +43,7 @@ async def serve(q: Q):
     )
 
     if not q.client.model_selected:    # 初期画面。モデル選択前
-        list_model_path = os.listdir(os.path.join('..', 'models'))
+        list_model_path = os.listdir(os.path.join('..', 'models'))    # modelsディレクトリのモデル一覧
         choices = [ui.choice(mod, mod) for mod in list_model_path]
         q.page['card_model_select'] = ui.form_card(
             box='1 2 2 10',    # x座標 y座標 幅 高さ
@@ -51,7 +54,7 @@ async def serve(q: Q):
             ]
         )
     
-    await handle_on(q)     # 各ボタンが押された場合、発動
+    await handle_on(q)     # 各ボタンが押された場合、ここでハンドリング
 
     print("q.args --> ", q.args)
     print("q.client --> ", q.client)
@@ -103,9 +106,10 @@ async def do_scoring(q: Q):
     with zipfile.ZipFile(local_path_zip) as existing_zip:
         existing_zip.extractall(q.client.data_path_input)
 
-    # 解凍フォルダのパス
-    q.client.data_path_input_folder = os.path.join(q.client.data_path_input, os.path.splitext(os.path.basename(local_path_zip))[0])
-    
+    q.client.data_path_input_folder = os.path.join(q.client.data_path_input, os.path.splitext(os.path.basename(local_path_zip))[0])    # zip解凍後フォルダのパス（写真が入っているフォルダ）
+    q.client.data_path_output_folder = os.path.join(q.client.data_path_output, os.path.splitext(os.path.basename(local_path_zip))[0])  # 結果保存用（上と同じフォルダ名で作成）
+    os.makedirs(q.client.data_path_output_folder, exist_ok=True)
+
     res = run_python_scoring_pipeline()   # PyScoringの実行
     print(res)
 
