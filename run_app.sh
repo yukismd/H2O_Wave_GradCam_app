0a5c3ada785556cbef586c43c15cef1b78d17ab8
yukismd <yuki.smd@gmail.com>
2021-08-23 00:38:10 +0000

    test

--- run_app.sh
+++ run_app.sh
@@ -4,14 +4,14 @@
 rm -rf $HOME/wave-0.17.0-linux-amd64/data/f/*
 
 # Waveサーバの起動
-cd wave-0.17.0-linux-amd64
+cd $HOME/wave-0.17.0-linux-amd64
 nohup ./waved &
 echo "Wave server UP!!"
 
 # アプリの起動
-cd ~/wave_app
-nohup wave0130/bin/python sys_monitoring2.py &
-nohup wave0130/bin/wave run app.py &
+cd $HOME/H2O_Wave_GradCam_app/wave_app
+nohup env_wave0170/bin/python sys_monitoring2.py &
+nohup env_wave0170/bin/wave run app.py &
 
 echo "App is running!!"
 
